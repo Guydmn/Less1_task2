fcbb30722892abc0fdc71ad5bf5f4541c6e1543f
Roman Guydmn <romanguydmn@gmail.com>
2023-01-20 17:51:51 +0400

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int y = a * a;
+if (y == b)
+Console.Write($"b является квадратом числа a");
+else
+Console.Write($"b не является квадратом числа a");
